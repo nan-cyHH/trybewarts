93e28ba9f5e84dedddd706fc64b4cc49ee2da510
Nancy Schaal <nan_cy29@hotmail.com>
2023-05-30 02:08:42 +0200

    exercício 15 executado

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function loginHeader() {
 }
 
 btnLogin.addEventListener('click', loginHeader);
+
+const textarea = document.getElementById('comment');
+const characterCount = document.getElementById('character-count');
+
+function limitTextarea(element, maxLength) {
+  if (element.value.length > maxLength) {
+    element.value = element.value.slice(0, maxLength);
+  }
+}
+
+textarea.addEventListener('input', () => {
+  const maxLength = 500;
+  const currentLength = textarea.value.length;
+  const remainingLength = maxLength - currentLength;
+  characterCount.textContent = `Caracteres restantes: ${remainingLength}`;
+
+  limitTextarea(textarea, maxLength); // Chama a função limitTextarea para limitar o número máximo de caracteres
+});
